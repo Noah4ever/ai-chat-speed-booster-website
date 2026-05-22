711a7528aa97c96a213db812f0831dd4320d0600
Noah Thiering <noah.thiering@gmail.com>
2026-05-22 10:37:20 +0200

    perf: resize favicon and logo to their displayed size

--- src/lib/logos.ts
+++ src/lib/logos.ts
@@ -1,7 +1,7 @@
 import chrome from "../assets/logos/chrome.svg";
 import firefox from "../assets/logos/firefox.svg";
 import edge from "../assets/logos/edge.svg";
-import extensionIcon from "../assets/logos/icon-128.png";
+import extensionIcon from "../assets/logos/icon-64.png";
 
 export const browserLogos = { chrome, firefox, edge };
 export { extensionIcon };
